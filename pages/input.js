fe02e34ec8e2739f86579d572792447300091f56
harada <hharada@vitalify.jp>
2023-11-28 10:38:25 +0900

    cookie追加

--- pages/input.js
+++ pages/input.js
@@ -8,6 +8,7 @@ import  Tab from '@mui/material/Tab';
 import  Tabs from '@mui/material/Tabs';
 import { Box,Grid,Button,Link } from '@mui/material';
 import { useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 import { DateTime } from 'luxon';
 
@@ -43,12 +44,33 @@ function TabPanel(props) {
 const allTasks = constants.allTasks
 
 
-
 export default function InputPage() {
+
+    //cookieの処理
+    const [cookies, setCookies] = useCookies(['cookieId']);
+    
+    //cookieによる初回か2回目以降かの判定
+    const cookieCheck = () => {
+        switch (cookies.cookieId) {
+          //初回のユーザー
+          default:
+              setCookies("cookieId", 10000);
+              break
+              
+          //2回目のユーザー
+          case 10000:
+              setCookies("cookieId", 20000);
+              break;
+              
+          //3回目以降のユーザー
+          case 20000:
+            break;
+        }
+      };
 
     const [ currentTab, setCurrentTab ] = useState(0);
     const [ currentTaskRepartition, setAllTaskRepartition ] = useState(getInitialTaskRepartition());
-
+    
     const getAllInputComponents = (taskArray, personKey) => {
 
         const returnArray = [];
@@ -194,6 +216,16 @@ export default function InputPage() {
                         setCurrentTab(currentTab + 1);
                         scrollToTop();
                     }}>次へ</Button>
+                {currentTab === 2 ? 
+                // <Button href='/result' color="primary" variant="contained" onClick={cookieCheck(cookies, setCookies)} disabled={currentTab === 1}>
+                <Button href='/result' color="primary" variant="contained" onClick={()=>cookieCheck()} disabled={currentTab === 1}>
+                    {/* <Link 
+                    as={`/result`}
+                    href={{pathbane:`/result`, query:info}}
+                    >この内容で診断する</Link> */}
+                    この内容で診断する
+                </Button> 
+                : ''}
                 </Grid>
             </Grid>
         </div>
