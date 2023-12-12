79ad9ffa94c7492ce5240c4d1bfb6267938e7749
harada <hharada@vitalify.jp>
2023-12-12 15:16:52 +0900

    fix: 家事分担リスト更新の処理追加

--- app/pages/input.jsx
+++ app/pages/input.jsx
@@ -132,7 +132,7 @@ export default function InputPage() {
         //     currentTaskRepartition['myTasks'][taskName].participates = !taskRepartitionItem.participates;
         // }
 
-        // setAllTaskRepartition(currentTaskRepartition);
+        setAllTaskRepartition(currentTaskRepartition);
 
     }
     
