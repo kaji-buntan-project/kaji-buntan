1e4daec9abb594ec003625a19b6cac1d0a7ca00a
yokoyama <yokoyama@miraishare.jp>
2023-12-14 16:34:49 +0900

    makeAliceBobUtilityの修正第一ステップ

--- app/src/calculateBurden.js
+++ app/src/calculateBurden.js
@@ -1,3 +1,4 @@
-export default function calculateBurden(effort, duration,participates){
-    return (2 - effort) * (duration * participates);
+export default function calculateBurden(effort, duration){
+    // ここでは一単位ごとの負担度を計算するため，回数はかけない.
+    return (2 - effort) * (duration);
 }
--- app/src/mainAlgorithm.js
+++ app/src/mainAlgorithm.js
@@ -376,34 +376,33 @@ function leastChangeAllocation(aliceUtility,bobUtility,aliceAllocation, bobAlloc
 
 
 export default function makeAliceBobUtility(allTasks, currentTaskRepartition){
-    let aliceUtility = [];
-    let bobUtility = [];
-    let aliceAllocation = [];
-    let bobAllocation = [];
-    let taskList = [];
+    let aliceBurdenDict = {}; // dict {'task.name': number} 各家事に対する負担度(1単位)
+    let bobBurdenDict = {}; // dict {'task.name': number} 各家事に対する負担度(1単位)
+    let aliceAllocationDict = {}; // dict {'task.name': number} 各家事の回数
+    let bobAllocationDict = {}; // dict {'task.name': number} 各家事の回数
+    let taskList = []; // string[] // task.checked==true の家事リスト
+
     for (let category of allTasks){
         for (let task of category.children){
             if (task.checked){
                 const myTask1 = currentTaskRepartition['myTasks'][task.name];
-                aliceUtility.push(calculateBurden(myTask1.effort, myTask1.duration, myTask1.participates));
+                aliceBurdenDict[task.name] = calculateBurden(myTask1.effort, myTask1.duration);
+                aliceAllocationDict[task.name] = myTask1.participates;
+                // 0回も辞書に加える.
+
                 const partnerTask1 = currentTaskRepartition['partnerTasks'][task.name];
-                bobUtility.push(calculateBurden(partnerTask1.effort, partnerTask1.duration, partnerTask1.participates));
+                bobBurdenDict[task.name] = calculateBurden(partnerTask1.effort, partnerTask1.duration);
+                bobAllocationDict[task.name] = partnerTask1.participates;
+                // 0回も辞書に加える.
+
                 taskList.push(task.name);
-                //console.log(myTask1);
-                if (myTask1 && myTask1.participates){
-                    aliceAllocation.push(task.name);
-                }
-                if (partnerTask1 && partnerTask1.participates){
-                    bobAllocation.push(task.name);
-                }
             }
         }
     }
 
-    let adjustedWinnerTaskRepartition = improvedAdjustedWinner(aliceUtility,bobUtility,taskList,currentTaskRepartition);
-
-    let leastChangeAllocationTaskRepartition = leastChangeAllocation(aliceUtility,bobUtility,aliceAllocation, bobAllocation,taskList,currentTaskRepartition);
-
+    let adjustedWinnerTaskRepartition = improvedAdjustedWinner(aliceBurdenDict,bobBurdenDict,taskList,currentTaskRepartition);
+    let leastChangeAllocationTaskRepartition = leastChangeAllocation(aliceBurdenDict, bobBurdenDict, aliceAllocationDict, bobAllocationDict, taskList, currentTaskRepartition);
+    
     return [adjustedWinnerTaskRepartition, leastChangeAllocationTaskRepartition];
 }
 
