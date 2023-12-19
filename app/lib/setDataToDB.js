2a5c6f13a8f7320d0e4a47de65718cf5723ea16d
harada <hharada@vitalify.jp>
2023-12-19 12:44:47 +0900

    fix: API送信用にデータを整形

--- app/lib/setDataToDB.js
+++ app/lib/setDataToDB.js
@@ -1,5 +1,5 @@
 import { useAtom } from "jotai";
-import { currentTaskRepartitionAtom, leastRepartitionAtom, adjustedRepartitionAtom, allTasksAtom } from "../lib/atoms.js";
+import { currentTaskRepartitionAtom, leastRepartitionAtom, adjustedRepartitionAtom } from "../lib/atoms.js";
 
 export const setDataToDB = () => {
   //現在の家事分担
@@ -29,24 +29,62 @@ export const setDataToDB = () => {
   const adjustedRepartitionPartnerTask = adjustedRepartition.partnerTasks;
   const adjustedRepartitionPartnerData = Object.fromEntries(Object.entries(adjustedRepartitionPartnerTask).filter(([key, value]) => value.userModified === true));
 
-  //送信するデータ
-  const data = {};
+  //送信するデータ（例）
+  // data = {
+  //     houseworks: [
+  //         {
+  //             name: ”朝ごはん用意”,
+  //             category: ”朝の準備”,
+  //             myEffort: 3,
+  //             myDuration: 30,
+  //             partnerEffort: 1,
+  //             partnerDuration: 20,
+  //             myNowParticipates: 4,
+  //             partnerNowParticipates: 2,
+  //             myLeastRepartitionParticipates: 3,
+  //             partnerLeastRepartitionParticipates: 3,
+  //             myAdjustedRepartitionParticipates: 3,
+  //             partnerAdjustedRepartitionParticipates: 3
+  //         },
+  //         {
+  //             name: ”新聞をとる”,
+  //             category: ”朝の準備”,
+  //             myEffort: 3,
+  //             myDuration: 30,
+  //             partnerEffort: 1,
+  //             partnerDuration: 20,
+  //             myNowParticipates: 4,
+  //             partnerNowParticipates: 2,
+  //             myLeastRepartitionParticipates: 3,
+  //             partnerLeastRepartitionParticipates: 3,
+  //             myAdjustedRepartitionParticipates: 3,
+  //             partnerAdjustedRepartitionParticipates: 3
+  //         }
+  //     ]
+  // }
 
-  for (const houseWorks of Object.keys(myData)) {
-    data[houseWorks] = {
-      category: myData[houseWorks].category, // 家事の分類
-      myEffort: Number(2 - myData[houseWorks].effort), // 「私」のその家事の評価
-      myDuration: myData[houseWorks].duration, // 「私」のその家事にかかる時間
-      partnerEffort: Number(2 -partnerData[houseWorks].effort), // 「パートナー」のその家事の評価
-      partnerDuration: partnerData[houseWorks].duration, // 「パートナー」がその家事にかかる時間
-      myNowParticipates: myData[houseWorks].participates, // 今の「私」の分担回数
-      PartnerNowParticipates: partnerData[houseWorks].participates, // 今の「パートナー」の分担回数
-      myLeastRepartitionParticipates: leastRepartitionMyData[houseWorks].participates, // 「私」の少し理想的な分担回数
-      partnerLeastRepartitionParticipates: leastRepartitionPartnerData[houseWorks].participates, // 「パートナー」の少し理想的な分担回数
-      myAdjustedRepartitionParticipates: adjustedRepartitionMyData[houseWorks].participates, // 「私」の理想的な分担回数
-      partnerAdjustedRepartitionParticipates: adjustedRepartitionPartnerData[houseWorks].participates, // 「パートナー」の理想的な分担回数
+  let data = {};
+  let houseWorks = [];
+
+  for (const houseWork of Object.keys(myData)) {
+    houseWorks[houseWork] = {
+      name: [houseWork][0], //家事の名前
+      category: myData[houseWork].category, // 家事の分類
+      myEffort: Number(2 - myData[houseWork].effort), // 「私」のその家事の評価
+      myDuration: myData[houseWork].duration, // 「私」のその家事にかかる時間
+      partnerEffort: Number(2 - partnerData[houseWork].effort), // 「パートナー」のその家事の評価
+      partnerDuration: partnerData[houseWork].duration, // 「パートナー」がその家事にかかる時間
+      myNowParticipates: myData[houseWork].participates, // 今の「私」の分担回数
+      PartnerNowParticipates: partnerData[houseWork].participates, // 今の「パートナー」の分担回数
+      myLeastRepartitionParticipates: leastRepartitionMyData[houseWork].participates, // 「私」の少し理想的な分担回数
+      partnerLeastRepartitionParticipates: leastRepartitionPartnerData[houseWork].participates, // 「パートナー」の少し理想的な分担回数
+      myAdjustedRepartitionParticipates: adjustedRepartitionMyData[houseWork].participates, // 「私」の理想的な分担回数
+      partnerAdjustedRepartitionParticipates: adjustedRepartitionPartnerData[houseWork].participates, // 「パートナー」の理想的な分担回数
     };
   }
-  console.log(data);
+  //keyを外す
+  const newData = Object.values(houseWorks);
+  //送信するデータ（例）になるように挿入
+  data.houseWorks = [...newData];
   return data;
 };
