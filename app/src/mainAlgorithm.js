9b9d4bd082da932e884a23e997771d58ecc97c09
yokoyama <yokoyama@miraishare.jp>
2023-12-25 15:29:05 +0900

    アルゴリズムの入力と出力部分のパーサーを追加

--- app/src/mainAlgorithm.js
+++ app/src/mainAlgorithm.js
@@ -7,23 +7,28 @@
 // import leastChangeAllocation from "../src/leastChangeAllocation";
 
 import React, { useState, useEffect } from 'react';
-import { improved_adjusted_winner } from 'wasm_rust_project/pkg';
+import { improved_adjusted_winner, least_change_allocation } from 'wasm_rust_project/pkg';
+
+function getIndex(taskName, taskList) {
+    return taskList[taskName]
+}
+
 
 export default function makeAliceBobUtility(allTasks, currentTaskRepartition){
 
     // currentTaskRepartition: {'me': {participates: number, effort: number, duration: number, category: categoryObject.name, userModified: boolean}, 'partner':{participates: boolean, effort: number, duration: number, category: categoryObject.name, userModified: boolean}}
-    
-    // let aliceBurdenDict = {}; // dict {'task.name': number} 各家事に対する負担度(1単位)
-    // let bobBurdenDict = {}; // dict {'task.name': number} 各家事に対する負担度(1単位)
-    // let aliceAllocationDict = {}; // dict {'task.name': number} 各家事の回数
-    // let bobAllocationDict = {}; // dict {'task.name': number} 各家事の回数
-    // let taskDict = {}; // dict {'task.name': number}  task.checked==true になっている家事の総回数
 
-    // task_total_num_list: Vec<i32>, alice_burden_list: Vec<i32>, bob_burden_list: Vec<i32>, を作成
+
+    // task_total_num_list: Vec<i32>, alice_burden_list: Vec<i32>, bob_burden_list: Vec<i32>, current_alice_allocation: Vec<i32>, current_bob_allocation:Vec<i32> を作成
 
     let task_total_num_list = [];
     let alice_burden_list = [];
     let bob_burden_list = [];
+    let current_alice_allocation = [];
+    let current_bob_allocation = [];
+
+    let taskList = {};
+    let counter0 = 0
 
     for (let category of allTasks){
         for (let task of category.children){
@@ -31,19 +36,77 @@ export default function makeAliceBobUtility(allTasks, currentTaskRepartition){
                 task_total_num_list.push(task.myDefault + task.partnerDefault);
                 alice_burden_list.push(calculateBurden(currentTaskRepartition.myTasks[task.name].effort, currentTaskRepartition.myTasks[task.name].duration));
                 bob_burden_list.push(calculateBurden(currentTaskRepartition.partnerTask1[task.name].effort, currentTaskRepartition.partnerTask1[task.name].duration));
+                current_alice_allocation.push(currentTaskRepartition.myTasks[task.name].participates);
+                current_bob_allocation.push(currentTaskRepartition.myTasks[task.name].participates);
+
+                taskList[task.name] = counter0;
+                counter0 += 1;
             }
         }
     }
+
+    ////// wasm improved_adjusted_winner 
     useEffect(() => {
         // ここで wasm モジュールの関数を呼び出す
-        let alliceAllocation, bobAllocation = improved_adjusted_winner(task_total_num_list, alice_burden_list, bob_burden_list);
-        console.log(alliceAllocation);
-        console.log(bobAllocation);
+        // let adjustedWinnerAllocation = improved_adjusted_winner([7,7,7,7,7], [5,10,5,10,15], [1,2,3,4,5]);
+        let adjustedWinnerAllocation = improved_adjusted_winner(task_total_num_list, alice_burden_list, bob_burden_list); 
+        console.log("adjustedWinner_alliceAllocation: ", adjustedWinnerAllocation[0]);
+        console.log("adjustedWinner_bobAllocation: ", adjustedWinnerAllocation[1]);
     }, []);
+    //////
+    ////// wasm least_change_allocation
+    useEffect(() => {
+        // ここで wasm モジュールの関数を呼び出す
+        // let leastChangeAllocation = least_change_allocation([7,7,7,7,7], [5,10,5,10,15], [1,2,3,4,5], [3,3,3,3,3], [4,4,4,4,4]);
+        let leastChangeAllocation = least_change_allocation(task_total_num_list, alice_burden_list, bob_burden_list, current_alice_allocation, current_bob_allocation);
+        console.log("leastChange_alliceAllocation: ", leastChangeAllocation[0]);
+        console.log("leastChange_bobAllocation: ", leastChangeAllocation[1]);
+    }, []);
+    //////
 
-    // let adjustedWinnerTaskRepartition = improvedAdjustedWinner(aliceBurdenDict, bobBurdenDict, taskDict, currentTaskRepartition);
-    // let leastChangeAllocationTaskRepartition = leastChangeAllocation(aliceBurdenDict, bobBurdenDict, aliceAllocationDict, bobAllocationDict, taskDict, currentTaskRepartition);
-    
-    return (alliceAllocation, bobAllocation);
+    // adjustedWinnerAllocationやleastChangeAllocationを，currentTaskRepartitionの形に戻す必要がある.
+
+    let awCurrentTaskRepartition = {};
+    let lcCurrentTaskRepartition = {};
+
+    for (let category of allTasks){
+        for (let task of category.children){
+            if (task.checked){
+                let i = getIndex(task.name, taskList);
+                
+                awCurrentTaskRepartition['myTasks'] = {
+                    participates: adjustedWinnerAllocation[0][i],
+                    effort: currentTaskRepartition.myTasks[task.name].effort,
+                    duration : currentTaskRepartition.myTasks[task.name].duration,
+                    category : currentTaskRepartition.myTasks[task.name].category,
+                    userModified:currentTaskRepartition.myTasks[task.name].userModified,
+                };
+                awCurrentTaskRepartition['partnerTasks'] = {
+                    participates: adjustedWinnerAllocation[1][i],
+                    effort: currentTaskRepartition.myTasks[task.name].effort,
+                    duration : currentTaskRepartition.myTasks[task.name].duration,
+                    category : currentTaskRepartition.myTasks[task.name].category,
+                    userModified:currentTaskRepartition.myTasks[task.name].userModified,
+                };
+
+                lcCurrentTaskRepartition['myTasks'] = {
+                    participates: leastChangeAllocation[0][i],
+                    effort: currentTaskRepartition.myTasks[task.name].effort,
+                    duration : currentTaskRepartition.myTasks[task.name].duration,
+                    category : currentTaskRepartition.myTasks[task.name].category,
+                    userModified:currentTaskRepartition.myTasks[task.name].userModified,
+                };
+                lcCurrentTaskRepartition['partnerTasks'] = {
+                    participates: leastChangeAllocation[1][i],
+                    effort: currentTaskRepartition.myTasks[task.name].effort,
+                    duration : currentTaskRepartition.myTasks[task.name].duration,
+                    category : currentTaskRepartition.myTasks[task.name].category,
+                    userModified:currentTaskRepartition.myTasks[task.name].userModified,
+                };
+            }
+        }
+    }
+
+    return (awCurrentTaskRepartition, lcCurrentTaskRepartition);
 }
 
