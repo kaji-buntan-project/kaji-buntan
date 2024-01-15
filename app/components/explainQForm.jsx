8b48ca9027f9cad6a446a1edad0aa7a8a2f896ff
harada <hharada@vitalify.jp>
2024-01-15 19:07:11 +0900

    fix: 結果表示画面の下部にあるリンクにtarget="blank"がない

--- app/components/explainQForm.jsx
+++ app/components/explainQForm.jsx
@@ -15,9 +15,9 @@ export default function ExplainQForm() {
                 position: 'relative',
             }}>
                 <b className='text'><font size="3">{ "もっと知りたい方へ\n" }</font></b>
-                <b className='text'><font size="2" color="#9c27b0"><a href="https://drive.google.com/file/d/1VF6hrktuFPq-ggrXRL_iqQeUCKxWCzVk/view?usp=sharing">{ "→詳しい説明へ\n" }</a></font></b>
+                <b className='text'><font size="2" color="#9c27b0"><a href="https://drive.google.com/file/d/1VF6hrktuFPq-ggrXRL_iqQeUCKxWCzVk/view?usp=sharing" target="blank">{ "→詳しい説明へ\n" }</a></font></b>
                 <b className='text'><font size="3">{ "アンケートにご協力ください\n" }</font></b>
-                <b className='text'><font size="2" color="#9c27b0"><a href="https://forms.gle/kjHo2mRW1PpJEBE59">{ "→アンケートへ\n" }</a></font></b>
+                <b className='text'><font size="2" color="#9c27b0"><a href="https://forms.gle/kjHo2mRW1PpJEBE59" target="blank">{ "→アンケートへ\n" }</a></font></b>
                 <Box sx={{
                     boxSizing: 'content-box',
                     backgroundColor: 'transparent',
