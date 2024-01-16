55081c747bded6abd4e024118db07969bd3a4c4b
harada <hharada@vitalify.jp>
2024-01-16 15:02:57 +0900

    fix: id（cookie）発行を'/input'ページで実行する

--- app/pages/input.jsx
+++ app/pages/input.jsx
@@ -12,6 +12,9 @@ import InputBox from "../components/InputBox";
 import { Box,Grid,Button,Link } from '@mui/material';
 import { useState } from 'react';
 import { useRouter } from "next/router";
+import { getCookies, getCookie, setCookie } from 'cookies-next';
+//https://www.npmjs.com/package/uuid
+import { v4 as uuidv4 } from 'uuid';
 
 // import {leastChangeAllocationTaskRepartition,adjustedWinnerTaskRepartition} from '../lib/init.js'
 import {setDataToDB} from '../lib/setDataToDB'
@@ -29,6 +32,26 @@ import GuideTalk from 'components/guideTalk';
 import { useAtom } from "jotai";
 import NextLink from "next/link";
 import { currentTaskRepartitionAtom, leastRepartitionAtom, adjustedRepartitionAtom, allTasksAtom,validateErrorAtom } from "../lib/atoms.js";
+
+export const getServerSideProps = ({req, res}) =>{
+
+    const cookie = getCookie('cookie_id', { req, res });
+    console.log('cookie',cookie);
+  
+    if(cookie){
+      console.log('2回目以降のアクセス');
+      return { props: {} };
+  
+    } else {
+      console.log('1回目のアクセス');
+      const newCookie = uuidv4();
+      setCookie('cookie_id', newCookie, { req, res, maxAge: 60 * 60 * 24 * 365 });
+    }
+  
+    getCookies({ req, res });
+    
+    return { props: {} };
+  }
 
 // TabPanel -> https://mui.com/material-ui/react-tabs/
 function TabPanel(props) {
