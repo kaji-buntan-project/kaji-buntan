09a0dd300131945ac1f2afd4a18d61c26703a746
vf_tanpt <tanpt@vitalify.jp>
2024-01-18 11:52:10 +0900

    Config slack

--- api/app/Http/Controllers/Api/DiagnosisDataController.php
+++ api/app/Http/Controllers/Api/DiagnosisDataController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\JsonResponse;
 
 #[OpenApi\PathItem]
 #[OpenApi\Collection(['default'])]
@@ -30,46 +31,59 @@ class DiagnosisDataController extends Controller
     #[OpenApi\RequestBody(factory: PostDiagnosisDataRequestBody::class)]
     public function store(Request $request)
     {
-        // Validate the incoming JSON data
-        $data = $request->validate([
-            'houseworks' => 'required|array',
-            'houseworks.*.name' => 'required|string',
-            'houseworks.*.category' => 'required|string',
-            'houseworks.*.myEffort' => 'required|integer',
-            'houseworks.*.myDuration' => 'required|integer',
-            'houseworks.*.partnerEffort' => 'required|integer',
-            'houseworks.*.partnerDuration' => 'required|integer',
-            'houseworks.*.myNowParticipates' => 'required|integer',
-            'houseworks.*.partnerNowParticipates' => 'required|integer',
-            'houseworks.*.myLeastRepartitionParticipates' => 'required|integer',
-            'houseworks.*.partnerLeastRepartitionParticipates' => 'required|integer',
-            'houseworks.*.myAdjustedRepartitionParticipates' => 'required|integer',
-            'houseworks.*.partnerAdjustedRepartitionParticipates' => 'required|integer',
-        ]);
+        try {
+            // Validate the incoming JSON data
+            $data = $request->validate([
+                'houseworks' => 'required|array',
+                'houseworks.*.name' => 'required|string',
+                'houseworks.*.category' => 'required|string',
+                'houseworks.*.myEffort' => 'required|integer',
+                'houseworks.*.myDuration' => 'required|integer',
+                'houseworks.*.partnerEffort' => 'required|integer',
+                'houseworks.*.partnerDuration' => 'required|integer',
+                'houseworks.*.myNowParticipates' => 'required|integer',
+                'houseworks.*.partnerNowParticipates' => 'required|integer',
+                'houseworks.*.myLeastRepartitionParticipates' => 'required|integer',
+                'houseworks.*.partnerLeastRepartitionParticipates' => 'required|integer',
+                'houseworks.*.myAdjustedRepartitionParticipates' => 'required|integer',
+                'houseworks.*.partnerAdjustedRepartitionParticipates' => 'required|integer',
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            Log::error("Validation error for API URL: {$request->url()}.\n Errors: " . json_encode($e->errors(), JSON_UNESCAPED_UNICODE) . ".\n Request Parameters:: " . json_encode($request->all(), JSON_UNESCAPED_UNICODE));
+            return response()->json(['errors' => $e->errors()], JsonResponse::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
         try {
             DB::beginTransaction();
 
             // Get the authenticated user's app ID from the cookie
             $appId = Cookie::get('cookie_id');
-
+            if (!$appId) {
+                throw new ApiException("The App ID is null.", 400);
+            }
             // Use the DiagnosisDataService to store diagnosis data
             if ($this->diagnosisDataService->storeDiagnosisData($data, $appId)) {
                 DB::commit();
                 return response()->json(['message' => 'Diagnosis saved successfully'], 201);
             } else {
                 DB::rollBack();
-                return response()->json(['error' => 'Error saving diagnosis'], 500);
+                throw new ApiException("Error saving diagnosis", 500);
             }
         }
         catch (ApiException $e) {
             DB::rollBack();
+            Log::error("Error for API URL: {$request->url()}.\n Errors: " . json_encode($e->getMessage(), JSON_UNESCAPED_UNICODE) . ".\n Request Parameters:: " . json_encode($request->all()));
+            return response()->json(['error' => 'Error saving diagnosis'], 500);
             throw $e;
         }
         catch (\Exception $e) {
             DB::rollBack();
-            Log::error($e);
-            // Handle the exception, log or respond with an error message
+            Log::error("Error for API URL: {$request->url()}.\n Request Parameters:: " . json_encode($request->all(), JSON_UNESCAPED_UNICODE) . ".\n" . $e->__toString());
+            return response()->json(['error' => 'Error saving diagnosis'], 500);
+        }
+        catch (\Throwable $e) {
+            DB::rollBack();
+            Log::error("Error for API URL: {$request->url()}.\n Request Parameters:: " . json_encode($request->all(), JSON_UNESCAPED_UNICODE) . ".\n" . $e->__toString());
             return response()->json(['error' => 'Error saving diagnosis'], 500);
         }
     }
--- api/config/logging.php
+++ api/config/logging.php
@@ -54,7 +54,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'slack'],
             'ignore_exceptions' => false,
         ],
 
@@ -76,7 +76,7 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
+            'username' => '家事分担コンシェルジュログ: ' . env('APP_URL'),
             'emoji' => ':boom:',
             'level' => env('LOG_LEVEL', 'critical'),
             'replace_placeholders' => true,
