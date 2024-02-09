0b72e62aa30a4498fa227797f85decdf2d0dbb35
harada <hharada@vitalify.jp>
2024-02-09 19:12:39 +0900

    feat: 時間スライダー変更 (todo:) 関数のリファクタリング 「分後」をつける

--- app/components/newInputItem.jsx
+++ app/components/newInputItem.jsx
@@ -22,23 +22,179 @@ export default function InputItem(props) {
     const [ isDoingTask, setDoingTask ] = useState(initialValue.participates);
     const [ happyLevel, setHappyLevel ] = useState(initialValue.effort ? initialValue.effort : 0); // Neutral: 0, Unhappy: -1, Happy: +1
     const [ taskTime, setTaskTime ] = useState(initialValue.duration ? initialValue.duration : 30);
+    const [ sliderValue , setSliderValue] = useState(taskTime)
+
+    useEffect(()=>{
+        changeSliderValueToTaskTime(sliderValue)
+    },[sliderValue])
+
+    useEffect(()=>{
+        changeTaskTimeToSliderValue(taskTime)
+    },[])
+    
+    const changeSliderValueToTaskTime = (sliderValue) => {
+        switch(sliderValue){
+            case 1:
+            setTaskTime(1)
+            break
+
+            case 5:
+            setTaskTime(3)
+            break
+
+            case 15:
+            setTaskTime(5)
+            break
+
+            case 30:
+            setTaskTime(10)
+            break
+
+            case 40:
+            setTaskTime(20)
+            break
+
+            case 50:
+            setTaskTime(30)
+            break
+
+            case 60:
+            setTaskTime(40)
+            break
+
+            case 67:
+            setTaskTime(50)
+            break
+
+            case 75:
+            setTaskTime(60)
+            break
+
+            case 80:
+            setTaskTime(70)
+            break
+
+            case 90:
+            setTaskTime(90)
+            break
+        }
+    }
+
+    const changeTaskTimeToSliderValue= (taskTime) => {
+        switch(taskTime){
+            case 1:
+            setSliderValue(1)
+            break
+
+            case 3:
+            setSliderValue(5)
+            break
+
+            case 5:
+            setSliderValue(15)
+            break
+
+            case 10:
+            setSliderValue(30)
+            break
+
+            case 20:
+            setSliderValue(40)
+            break
+
+            case 30:
+            setSliderValue(50)
+            break
+
+            case 40:
+            setSliderValue(60)
+            break
+
+            case 50:
+            setSliderValue(67)
+            break
+
+            case 60:
+            setSliderValue(75)
+            break
+
+            case 70:
+            setSliderValue(80)
+            break
+
+            case 90:
+            setSliderValue(90)
+            break
+        }
+    }
+
+    // const sliderMarks = [
+    //     {
+    //         value: 10,
+    //         label: '10分',
+    //     },
+    //     {
+    //         value: 30,
+    //         label: '30分',
+    //     },
+    //     {
+    //         value: 60,
+    //         label: '60分',
+    //     },
+    //     {
+    //         value: 90,
+    //         label: '90分',
+    //     }
+    // ];
     
     const sliderMarks = [
         {
-            value: 10,
-            label: '10分',
+            value: 1,
+            label: '1',
+        },
+        {
+            value: 5,
+            // label: '3',
+        },
+        {
+            value: 15,
+            label: '5',
         },
         {
             value: 30,
-            label: '30分',
+            label: '10',
+        },
+        {
+            value: 40,
+            // label: '20分',
+        },
+        {
+            value: 50,
+            label: '30',
         },
         {
             value: 60,
-            label: '60分',
+            // label: '40分',
+        },
+        {
+            value: 67,
+            // label: '50分',
+        },
+        {
+            value: 75,
+            label: '60',
+        },
+        {
+            value: 80,
+            // label: '70分',
+        },
+        {
+            value: 85,
+            // label: '80分',
         },
         {
             value: 90,
-            label: '90分',
+            label: '90',
         }
     ];
 
@@ -96,13 +252,13 @@ export default function InputItem(props) {
 
         <Slider
             className={ styles.slider }
-            value={ taskTime }
+            value={ sliderValue }
             sx={{ gridArea: 'duration', marginLeft: '1em' }}
-            step={10}
+            step={null}
             marks= { sliderMarks }
-            min={10}
+            min={1}
             max={90}
-            onChange={ (_, newValue) => setTaskTime(newValue) }
+            onChange={ (_, newValue) => setSliderValue(newValue) }
             />
         </div>
         </>
